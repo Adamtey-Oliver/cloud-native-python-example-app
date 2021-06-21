ea6063d1537bb9b9ab95ca11650eefb844fd6f55
Eric Wright <ericwright@erichwright-mac.fios-router.home>
2021-06-21 15:03:10 -0400

    updated content

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "This works, which is surprising sometimes"
+    return "This works, which is surprising sometimes. Now with 100% more DevOps!"
  
 
 @app.route("/healthz")
